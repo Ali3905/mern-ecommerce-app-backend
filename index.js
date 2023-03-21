094c6312d51a1d0255ea8620fcb672efa21a7c76
Ali3905 <muhammadali30905@gmail.com>
2023-03-21 19:55:56 +0500

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const connectToMongo = require("./db")
 const express = require("express")
 const cors = require("cors")
-const path = require("path")
+// const path = require("path")
 
 connectToMongo();
 const app = express();
@@ -15,11 +15,15 @@ app.use("/api", require("./routes/address"))
 app.use("/api", require("./routes/order"))
 app.use("/api", require("./routes/user"))
 
-app.use(express.static(path.join(__dirname, "./client/build")))
+app.get("/", (req, res) => {
+    res.send("Home page");
+  });
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+// app.use(express.static(path.join(__dirname, "./client/build")))
+
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, './client/build/index.html'))
+// })
 
 app.listen(8000, ()=>{
     console.log("Server is running on localhost: 8000" );
